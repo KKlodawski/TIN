9009ff395486ef61ac2c1fe02e92a65a598b4897
KKłodawski <s24777@pjwstk.edu.pl>
2023-12-06 18:50:51 +0100

    tin9 pre1

---
+++ tin9/s24777/src/App.js
@@ -0,0 +1,17 @@
+import './Styles/App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./Pages/Layout";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
